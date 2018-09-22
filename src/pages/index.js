b8e0c4336a381f9f0c1598e29d2e61fe8217e38e
julienr2 <julien.rougeron@gmail.com>
2018-09-21 19:43:45 -0700

    import more images

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,21 @@ import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 
+import styles from './index.module.scss';
+import googlePlayBadge from "../assets/images/google-play-badge.png"
+
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>
+      Trust your friends<br />
+      Go Travel<br />
+      Experience the World<br />
+    </h1>
+    <h2>
+      Nowmad is an App that lets you share with your friends your best experiences and discover new ways of enjoing the world around you.
+    </h2>
+    <img className={styles.image} src={googlePlayBadge} alt="Google Play Badge" />
+    <p>You can get notify when the IOS version is released.</p>
   </Layout>
 )
 
